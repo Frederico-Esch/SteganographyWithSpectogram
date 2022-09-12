294cb2701d11e87323a356ddc247744d9643baeb
Frederico <fredeschpe@gmail.com>
2022-09-12 10:11:25 -0300

    [MAJOR CHANGE] moved from stb to cv

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
-#include "Image.h"
-#include <algorithm>
-#include <cstring>
+#include <iostream>
+#include <cstdint>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+typedef std::string string;
 const string filename = "../robot.png";
 
+/* CONTRAST MODE
+ * for(size_t i = 0; i < size; i++){
+ *   data[i] = static_cast<double >(image[i])/max;
+ *   data[i] = 1/(data[i] + CONTRAST_PRECISION);
+ * }
+ */
 
 int main() {
-
-    Image image(filename, 1); //We need it to be grey
-    Manipulator mani(image, Manipulator::contrast_mode);
-    mani.normalize();
-    mani.remove_low(0.5);
-    image.contrast();
-
-    image.write("../image_contrast.png");
-    mani.write("../manipulator_contrast.png");
-
+    cv::Mat image = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+    image = 1/image;
+    cv::imwrite("../result.png", image);
     return 0;
 }
