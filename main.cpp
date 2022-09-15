adb2b00e3faf19edafe764e528cb5fae3c756ab8
Frederico <fredeschpe@gmail.com>
2022-09-15 17:27:57 -0300

    [UPDATE]

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <cstdint>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#define CONTRAST_SCALE_MAX 5e7
 
 typedef std::string string;
 const string filename = "../robot.png";
@@ -15,7 +17,33 @@ const string filename = "../robot.png";
 
 int main() {
     cv::Mat image = cv::imread(filename, cv::IMREAD_GRAYSCALE);
-    image = 1/image;
+    image.convertTo(image, CV_64FC1);
+
+    //cv::invert()
+    double max, min;
+    cv::minMaxLoc(image, &min, &max);
+    std::cout << min << " " << max << std::endl;
+
+    //cv::normalize(image, image, 1, 0, cv::NORM_MINMAX);
+    //image = 1/(image + CONTRAST_SCALE_MAX); //make
+    image.forEach<double>([&](double& p, const int * pos) -> void {
+        p = p/max;
+        p = 1/(p + CONTRAST_SCALE_MAX); // TODO UNDERSTAND  WHY IT  ISN'T WORKING LIKE A DOUBLE WOULD
+    });
+    cv::normalize(image, image, 1, 0, cv::NORM_MINMAX);
+
+    image.forEach<double>([](auto& p, const int * pos) -> void {
+        if(p < .5) p = 0;
+    });
+
+    cv::resize(image, image, cv::Size(1000, 1000));
+
+    cv::namedWindow("IDK", 0);
+    cv::imshow("IDK", image);
+    cv::waitKey(0);
+
+    cv::normalize(image, image, 255, 0, cv::NORM_MINMAX);
     cv::imwrite("../result.png", image);
+
     return 0;
 }
